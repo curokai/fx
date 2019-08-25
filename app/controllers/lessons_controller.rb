be244477f7a3bc759eddd9706ff8ededafb541fe
curokai <anadan20182019@gmail.com>
2019-08-25 00:12:26 +0000

    Permissions complete

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_authorized_for_current_course, only: [:show]
   def show
   end
 
   private
+
+  def require_authorized_for_current_course
+  if current_lesson.section.course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+      
+  end
+  end
 
   helper_method :current_lesson
   def current_lesson
